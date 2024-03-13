8b200be8c24715588f6f7d1b78d3056b362a2ace
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-13 22:21:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//Q25
+// Conditional
+// let alien_color = "green"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+// let alien_color = "red"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+//Q26
+// if-else chain
+// run the if block
+// let alien_color = "green"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points for shooting the alien!")}
+//   else{
+// console.log("you just earned 10 points.")
+//   }
+// run the else block
+// let alien_color = "red"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+//   else{
+// console.log("you just earned 10 points.")
+//   }
+//Q27
+var alien_color = "red";
+if (alien_color = "green") {
+    console.log("you just earned 5 points!");
+}
+else if (alien_color == "red") {
+    console.log("you just earned 10 points.");
+}
+else if (alien_color == "yellow") {
+    console.log("you just earned 15 points.");
+}
---
+++ main.ts
@@ -0,0 +1,59 @@
+//Q25
+// Conditional
+// let alien_color = "green"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+
+// let alien_color = "red"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+//Q26
+// if-else chain
+// run the if block
+// let alien_color = "green"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points for shooting the alien!")}
+//   else{
+    // console.log("you just earned 10 points.")
+//   }
+// run the else block
+// let alien_color = "red"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+//   else{
+    // console.log("you just earned 10 points.")
+//   }
+//Q27
+// run the green alien
+// let alien_color = "green"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+// else if(alien_color=="red"){
+    // console.log("you just earned 10 points.")
+// }
+// else if (alien_color == "yellow"){
+    // console.log("you just earned 15 points.")
+// }
+// run the red alien
+// let alien_color = "red"
+// if(alien_color="green"){
+//   console.log("you just earned 5 points!")}
+//   else if (alien_color=="red"){
+    // console.log("you just earned 10 points.")
+//   }
+//   else if (alien_color == "yellow"){
+    // console.log("you just earned 15 points.")
+//   }
+// run the yellow alien
+//  let alien_color = "yellow"
+// if(alien_color ="green"){
+    // console.log("you just earned 5 points!")}
+    // else if(alien_color =="red"){
+        // console.log("you just earned 10 points.")
+    // }
+    // else if (alien_color == "yellow"){
+        // console.log("you just earned 15 points.")
+    // }
+
+
+
